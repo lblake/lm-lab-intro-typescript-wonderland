a28e41e89fbe305aa896303dbf54e65d89d582c5
lblake <treleven.lloyd@gmail.com>
2023-01-09 14:54:35 +0000

    fixed code for activity 2

--- src/2_cheshire_cat.ts
+++ src/2_cheshire_cat.ts
@@ -3,39 +3,41 @@ import { askQuestion, clear, print } from '../console';
 import { attendATeaParty } from './3_tea_party';
 
 const expressions = [
-	'☹ Frown ☹',
-	'🙂 Smile 🙂',
-	'😁 Grin 😁',
-	'😶 Blank Face 😶',
-	'😡 Angry Face 😡',
+  '☹ Frown ☹',
+  '🙂 Smile 🙂',
+  '😁 Grin 😁',
+  '😶 Blank Face 😶',
+  '😡 Angry Face 😡',
 ] as const; // 💡 CURIOUS - see activity_2.md for explanations
 type FacialExpression = typeof expressions[number];
 
 class CheshireCat {
-	name: string = 'The Cheshire Cat';
-	expression: FacialExpression = expressions[0];
+  name: string = 'The Cheshire Cat';
+  expression: FacialExpression = expressions[0];
 }
 
 export function meetTheCheshireCat(): void {
-	clear(true);
+  clear(true);
 
-	const theCat = new CheshireCat();
+  const theCat = new CheshireCat();
 
-	// 👉 FIXME ❌
-	// 		Add some code here which allows you to progress *without* altering any other code in this file
-	// 👉 END FIXME ❌
+  theCat.expression = '😁 Grin 😁';
 
-	print(
-		`You can see ${theCat.name}. A wide ${theCat.expression} is hovering in the air.`
-	);
+  // 👉 FIXME ❌
+  // 		Add some code here which allows you to progress *without* altering any other code in this file
+  // 👉 END FIXME ❌
 
-	if (theCat.expression === '😁 Grin 😁') {
-		print(
-			`🥳 The cat allows you to continue onwards. There is the sound of a party in the distance... 🥳 `
-		);
-		return askQuestion('Press ENTER to continue! ', attendATeaParty);
-	} else {
-		print(`😱 The cat will not allow you to pass. 😱 `);
-		return endAdventure();
-	}
+  print(
+    `You can see ${theCat.name}. A wide ${theCat.expression} is hovering in the air.`
+  );
+
+  if (theCat.expression === '😁 Grin 😁') {
+    print(
+      `🥳 The cat allows you to continue onwards. There is the sound of a party in the distance... 🥳 `
+    );
+    return askQuestion('Press ENTER to continue! ', attendATeaParty);
+  } else {
+    print(`😱 The cat will not allow you to pass. 😱 `);
+    return endAdventure();
+  }
 }
